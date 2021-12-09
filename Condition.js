a87124cb83aada21bf51d5ec4ec193da33c036a8
mohibuslc <mohib_uslc@outlook.com>
2021-12-09 22:00:53 +0000

    Mathmatical tram

---
+++ Condition.js
@@ -0,0 +1,17 @@
+
+var Bis = 8;
+
+if (Bis <10){
+
+    console.log ('i do eat Bis and drink Tea');
+
+
+}
+
+else{
+
+    console.log('i do drink only Tea');
+
+
+
+}
---
+++ Math-tram.js
@@ -0,0 +1,21 @@
+var price1 = 10 ;
+price1--
+var price2 = 20 ;
+ price2++
+var total = price1 + price2;
+
+console.log(total);
+
+
+// work on String .... give white space middle of bouth string ;
+
+
+
+
+var price1 = ' kalam ';
+
+var name = ' jamal'
+
+var total = price1 + ' '+ name ;
+
+console.log(total);
